5732e1ac44a8ba7c49ceb95db816dfb8e265247e
Danny McGrory <dannyalaska@mac.com>
2015-11-19 15:16:59 -0500

    added artists to paintings

--- models/painting.js
+++ models/painting.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var PaintingSchema = new mongoose.Schema({
   title: String,
   img_url: String,
-  year_made: Date
+  year_made: Date,
+  artist: String
 });
 
 module.exports = Painting;
